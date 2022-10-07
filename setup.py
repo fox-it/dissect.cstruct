508c42987f2a9463b0587ec830c622f2c35e687a
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-07 17:05:52 +0200

    Update license acc. license file

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="Structure parsing in Python made easy.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license="MIT",
+    license="AGPLv3",
     keywords="cstruct struct dissect structure binary pack packer unpack unpacker parser parsing",
     url="https://github.com/fox-it/dissect.cstruct",
     packages=["dissect.cstruct", "dissect.cstruct.types"],
