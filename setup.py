efcd0a25072b6d645dcae4207c1f28c44e3553ed
Erik Schamper <1254028+Schamper@users.noreply.github.com>
2022-10-20 13:58:51 +0200

    Replace old setup.py (#11)
    (DIS-1610)

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
-from setuptools import setup
-
-with open("README.md") as fh:
-    long_description = fh.read()
+from setuptools import find_packages, setup
 
 setup(
     name="dissect.cstruct",
-    author="Fox-IT",
-    description="Structure parsing in Python made easy.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    keywords="cstruct struct dissect structure binary pack packer unpack unpacker parser parsing",
-    url="https://github.com/fox-it/dissect.cstruct",
-    packages=["dissect.cstruct", "dissect.cstruct.types"],
+    packages=list(map(lambda v: "dissect." + v, find_packages("dissect"))),
 )
