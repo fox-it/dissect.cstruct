a8d1b1193312f16df5e1eb56a07bd60547552418
Schamper <1254028+Schamper@users.noreply.github.com>
2023-08-31 14:58:28 +0200

    Fix tests

--- tests/test_expression.py
+++ tests/test_expression.py
@@ -1,6 +1,6 @@
 import pytest
-from dissect import cstruct
 
+from dissect.cstruct.cstruct import cstruct
 from dissect.cstruct.exceptions import ExpressionParserError, ExpressionTokenizerError
 from dissect.cstruct.expression import Expression
 
@@ -105,7 +105,7 @@ def test_expression_failure(expression: str, exception: type, message: str) -> N
         parser.evaluate()
 
 
-def test_sizeof():
+def test_sizeof(cs: cstruct):
     d = """
     struct test {
         char    a[sizeof(uint32)];
@@ -115,8 +115,7 @@ def test_sizeof():
         char    a[sizeof(test) * 2];
     };
     """
-    c = cstruct.cstruct()
-    c.load(d)
+    cs.load(d)
 
-    assert len(c.test) == 4
-    assert len(c.test2) == 8
+    assert len(cs.test) == 4
+    assert len(cs.test2) == 8
