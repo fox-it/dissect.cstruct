a0ab49727253c666751b0257a4d753eba36f1fb2
Schamper <1254028+Schamper@users.noreply.github.com>
2023-12-25 00:07:45 +0100

    Fix setting dynamic field for enums

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,5 @@
 from unittest.mock import Mock
 
-from dissect.cstruct import cstruct
 from dissect.cstruct.parser import TokenParser
 
 
@@ -24,20 +23,3 @@ def test_preserve_comment_newlines():
 
     mock_token.match.start.return_value = data.index("#define multi_anchor")
     assert TokenParser._lineno(mock_token) == 9
-
-
-def test_dynamic_substruct_size(cs: cstruct):
-    cdef = """
-    struct {
-        int32 len;
-        char str[len];
-    } sub;
-
-    struct {
-        sub data[1];
-    } test;
-    """
-    cs.load(cdef)
-
-    assert cs.sub.dynamic
-    assert cs.test.dynamic
