775f72a56fba6007ba4b32b0d099acef9b114596
Joost Jansen <12032793+joost-j@users.noreply.github.com>
2024-03-04 12:05:21 +0100

    Initial commit

--- dissect/cstruct/types/__init__.py
+++ dissect/cstruct/types/__init__.py
@@ -4,6 +4,7 @@ from dissect.cstruct.types.chartype import CharType
 from dissect.cstruct.types.enum import Enum, EnumInstance
 from dissect.cstruct.types.flag import Flag, FlagInstance
 from dissect.cstruct.types.instance import Instance
+from dissect.cstruct.types.leb128 import LEB128
 from dissect.cstruct.types.packedtype import PackedType
 from dissect.cstruct.types.pointer import Pointer, PointerInstance
 from dissect.cstruct.types.structure import Field, Structure, Union
@@ -21,6 +22,7 @@ __all__ = [
     "Flag",
     "FlagInstance",
     "Instance",
+    "LEB128",
     "PackedType",
     "Pointer",
     "PointerInstance",
