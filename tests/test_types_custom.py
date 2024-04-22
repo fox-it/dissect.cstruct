d05a2a741806c0fda7475826b165ef9ab387ba64
Miauwkeru <Miauwkeru@users.noreply.github.com>
2024-04-22 12:58:49 +0000

    Add custom type tests and example

---
+++ tests/test_types_custom.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+from typing import Any, BinaryIO
+
+from dissect.cstruct import cstruct
+from dissect.cstruct.types import BaseType, MetaType
+
+
+class EtwPointer(BaseType):
+    type: MetaType
+    size: int | None
+
+    @classmethod
+    def _read(cls, stream: BinaryIO, context: dict[str, Any] | None = None) -> BaseType:
+        return cls.type._read(stream, context)
+
+    @classmethod
+    def _read_0(cls, stream: BinaryIO, context: dict[str, Any] | None = None) -> list[BaseType]:
+        return cls.type._read_0(stream, context)
+
+    @classmethod
+    def _write(cls, stream: BinaryIO, data: Any) -> int:
+        return cls.type._write(stream, data)
+
+    @classmethod
+    def as_32bit(cls) -> None:
+        cls.type = cls.cs.uint32
+        cls.size = 4
+
+    @classmethod
+    def as_64bit(cls) -> None:
+        cls.type = cls.cs.uint64
+        cls.size = 8
+
+
+def test_adding_custom_type() -> None:
+    cs = cstruct()
+    cs.add_custom_type("EtwPointer", EtwPointer)
+
+    cs.EtwPointer.as_64bit()
+    assert cs.EtwPointer.type is cs.uint64
+    assert cs.EtwPointer.size == 8
+    assert cs.EtwPointer(b"\xDE\xAD\xBE\xEF" * 2).dumps() == b"\xDE\xAD\xBE\xEF" * 2
+
+    cs.EtwPointer.as_32bit()
+    assert cs.EtwPointer.type is cs.uint32
+    assert cs.EtwPointer.size == 4
+    assert cs.EtwPointer(b"\xDE\xAD\xBE\xEF" * 2).dumps() == b"\xDE\xAD\xBE\xEF"
+
+
+def test_using_type_in_struct() -> None:
+    cs = cstruct()
+    cs.add_custom_type("EtwPointer", EtwPointer)
+
+    struct_definition = """
+    struct test {
+        EtwPointer data;
+        uint64     data2;
+    };
+    """
+
+    cs.load(struct_definition)
+
+    cs.EtwPointer.as_64bit()
+    assert len(cs.test().data) == 8
+
+    cs.EtwPointer.as_32bit()
+    assert len(cs.test().data) == 4
