ca4b29ab8c11e6b4f9c434acc5e47ab250ea54ed
David Dorfman <daviddorfman42@gmail.com>
2024-05-06 15:59:36 +0300

    Ignore tabs in expression (#88)

--- dissect/cstruct/expression.py
+++ dissect/cstruct/expression.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import string
-from typing import TYPE_CHECKING, Callable, Optional, Union
+from typing import TYPE_CHECKING, Callable, Optional
 
 from dissect.cstruct.exceptions import ExpressionParserError, ExpressionTokenizerError
 
@@ -18,7 +18,7 @@ class ExpressionTokenizer:
         self.pos = 0
         self.tokens = []
 
-    def equal(self, token: str, expected: Union[str, str[str]]) -> bool:
+    def equal(self, token: str, expected: str | set[str]) -> bool:
         if isinstance(expected, set):
             return token in expected
         else:
@@ -42,7 +42,7 @@ class ExpressionTokenizer:
     def match(
         self,
         func: Optional[Callable[[str], bool]] = None,
-        expected: Optional[str] = None,
+        expected: Optional[str | set[str]] = None,
         consume: bool = True,
         append: bool = True,
     ) -> bool:
@@ -142,7 +142,7 @@ class ExpressionTokenizer:
                 self.tokens.append(">>")
             elif self.match(expected="<", append=False) and self.match(expected="<", append=False):
                 self.tokens.append("<<")
-            elif self.match(expected=" ", append=False):
+            elif self.match(expected={" ", "\t"}, append=False):
                 continue
             else:
                 raise ExpressionTokenizerError(
