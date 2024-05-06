9bbc3bac3b968612331e8a1baf38656d7df32b8d
Schamper <1254028+Schamper@users.noreply.github.com>
2024-05-06 16:54:45 +0200

    Update tests/utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -1,2 +1,7 @@
-def verify_compiled(struct, compiled):
+from __future__ import annotations
+
+from dissect.cstruct import Structure
+
+
+def verify_compiled(struct: type[Structure], compiled: bool) -> bool:
     return struct.__compiled__ == compiled
