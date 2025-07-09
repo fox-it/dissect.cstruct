36478855416633ff716da32f3d92416bc99fec1b
Andreas Kaeberlein <andreas.kaeberlein@siemens.com>
2025-07-09 09:46:05 +0200

    [tests/test_parser.py] fix Lint CI/CD fail, help: Replace with `cond is not None`

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -150,5 +150,5 @@ def test_includes(cs: cstruct) -> None:
     assert len(cs.includes) == 2
     assert cs.myStruct.__name__ == "myStruct"
     assert len(cs.typedefs["myStruct"].fields) == 1
-    assert cs.typedefs["myStruct"].fields.get("charVal") != None
+    assert not (cs.typedefs["myStruct"].fields.get("charVal") == None)
 
