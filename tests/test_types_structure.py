91f8de2e88067795d5e7003e455f1d476e5afe96
Schamper <1254028+Schamper@users.noreply.github.com>
2025-07-18 11:41:07 +0200

    Fix tests

--- tests/test_types_structure.py
+++ tests/test_types_structure.py
@@ -802,39 +802,6 @@ def test_codegen_make_init() -> None:
     assert result is cached
 
 
-def test_structure_definition_newline(cs: cstruct, compiled: bool) -> None:
-    cdef = """
-    struct test {
-        char    magic[4
-        ];
-
-        wchar   wmagic[4
-        ];
-        uint8   a;
-        uint16  b;
-        uint32  c;
-        char    string[];
-        wchar   wstring[];
-    };
-    """
-    cs.endian = ">"
-    cs.load(cdef, compiled=compiled)
-
-    assert verify_compiled(cs.test, compiled)
-
-    buf = b"test\x00t\x00e\x00s\x00t\x01\x02\x03\x04\x05\x06\x07lalala\x00\x00t\x00e\x00s\x00t\x00\x00"
-
-    obj = cs.test()
-    obj.magic = "test"
-    obj.wmagic = "test"
-    obj.a = 0x01
-    obj.b = 0x0203
-    obj.c = 0x04050607
-    obj.string = b"lalala"
-    obj.wstring = "test"
-
-    assert obj.dumps() == buf
-
 def test_codegen_hashable(cs: cstruct) -> None:
     hashable_fields = [Field("a", cs.uint8), Field("b", cs.uint8)]
     unhashable_fields = [Field("a", cs.uint8[2]), Field("b", cs.uint8)]
@@ -868,7 +835,7 @@ def test_structure_definition_newline(cs: cstruct, compiled: bool) -> None:
     buf = b"test\x00t\x00e\x00s\x00t\x01\x02\x03\x04\x05\x06\x07lalala\x00\x00t\x00e\x00s\x00t\x00\x00"
 
     obj = cs.test()
-    obj.magic = "test"
+    obj.magic = b"test"
     obj.wmagic = "test"
     obj.a = 0x01
     obj.b = 0x0203
