1af13da98bb79e3ec5fb15b73690cfe35d1e0700
Schamper <1254028+Schamper@users.noreply.github.com>
2026-02-26 14:29:21 +0100

    Add more benchmarks

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -12,6 +12,7 @@ if TYPE_CHECKING:
 
 @pytest.mark.benchmark
 def test_benchmark_basic(cs: cstruct, compiled: bool, benchmark: BenchmarkFixture) -> None:
+    """Benchmark the parsing of a simple struct with both the compiled and interpreted backends."""
     cdef = """
     struct test {
         uint32  a;
@@ -27,6 +28,7 @@ def test_benchmark_basic(cs: cstruct, compiled: bool, benchmark: BenchmarkFixtur
 
 @pytest.mark.benchmark
 def test_benchmark_union(cs: cstruct, compiled: bool, benchmark: BenchmarkFixture) -> None:
+    """Benchmark the parsing of a simple union with both the compiled and interpreted backends."""
     cdef = """
     union test {
         uint32  a;
@@ -42,6 +44,7 @@ def test_benchmark_union(cs: cstruct, compiled: bool, benchmark: BenchmarkFixtur
 
 @pytest.mark.benchmark
 def test_benchmark_attribute_access(cs: cstruct, benchmark: BenchmarkFixture) -> None:
+    """Benchmark the attribute access of a parsed struct."""
     cdef = """
     struct test {
         uint32  a;
@@ -54,3 +57,31 @@ def test_benchmark_attribute_access(cs: cstruct, benchmark: BenchmarkFixture) ->
     obj = cs.test(b"\x01\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x03\x00\x04")
 
     benchmark(lambda: (obj.a, obj.b, obj.c, obj.d))
+
+
+@pytest.mark.benchmark
+def test_benchmark_getattr_constants(cs: cstruct, benchmark: BenchmarkFixture) -> None:
+    """Benchmark the resolving of constants on the cstruct instance."""
+    cdef = """
+    #define CONST1 1
+    """
+    cs.load(cdef)
+
+    benchmark(lambda: cs.CONST1)
+
+
+@pytest.mark.benchmark
+def test_benchmark_getattr_types(cs: cstruct, benchmark: BenchmarkFixture) -> None:
+    """Benchmark the resolving of types on the cstruct instance."""
+    benchmark(lambda: cs.uint8)
+
+
+@pytest.mark.benchmark
+def test_benchmark_getattr_typedefs(cs: cstruct, benchmark: BenchmarkFixture) -> None:
+    """Benchmark the resolving of typedefs on the cstruct instance."""
+    cdef = """
+    typedef uint8 my_uint8;
+    """
+    cs.load(cdef)
+
+    benchmark(lambda: cs.my_uint8)
